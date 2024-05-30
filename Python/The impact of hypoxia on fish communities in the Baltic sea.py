4d3599718c83d40c11e816a6a60c0b7e118c295e
SurjusJ <juliette.surjus@etu.univ-amu.fr>
2024-05-30 15:29:51 +0200

    Add files via upload

--- Python/The impact of hypoxia on fish communities in the Baltic sea.py
+++ Python/The impact of hypoxia on fish communities in the Baltic sea.py
@@ -17,8 +17,8 @@ import matplotlib.colors as mcolors
 ############################## CALLING THE FILES ##############################
 ###############################################################################
 ##### LOCATION OF ALL THE DATA #####
-# LOCATION_Data=r"..\src\python" 
-LOCATION_Data=r"D:\JJJJJJJ\Documents\STAGE-EAWAG\Repository\batsea\src\python"  # Path for oxygen 
+# LOCATION_Data=r"..\Python" 
+LOCATION_Data=r"D:\JJJJJJJ\Documents\STAGE-EAWAG\BalticHypoxia-main\Python"    # Path for oxygen 
 
 ##### OPEN FILES ######
 # --> Oxygen
